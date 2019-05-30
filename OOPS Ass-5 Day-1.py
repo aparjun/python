a9a99c6896f8996440ca9af8af3529baa68a11b6
aparjun <46344050+aparjun@users.noreply.github.com>
2019-05-30 07:36:12 +0200

    Create OOPS Ass-5 Day-1.py

---
+++ OOPS Ass-5 Day-1.py
@@ -0,0 +1,18 @@
+#OOPR-Assgn-5
+#Start writing your code here
+class Vehicle:
+    def __init__(self):
+        self.vehicle_type=None
+        self.vehicle_cost=0
+        self.vehicle_id=0
+        self.premium_amount=0
+    def get_premium_amount(self,premium_amount):
+        self.premium_amount=premium_amount
+    def set_vehicle_type():
+        
+    def set_vehicle_cost:
+        
+    def set_premium_amount:
+        
+    def calculate_premium:
+        
